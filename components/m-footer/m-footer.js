09b01a1161f0654b8a0b046204ce78bdac7c92bb
pzhang <1063764688@qq.com>
2023-10-13 14:50:01 +0800

    feat: 底部组件实现

---
+++ components/m-footer/m-footer.js
@@ -0,0 +1,25 @@
+// components/m-footer/m-footer.js
+Component({
+
+  /**
+   * 组件的属性列表
+   */
+  properties: {
+
+  },
+
+  /**
+   * 组件的初始数据
+   */
+  data: {
+    year: new Date().getFullYear()
+
+  },
+
+  /**
+   * 组件的方法列表
+   */
+  methods: {
+
+  }
+})
