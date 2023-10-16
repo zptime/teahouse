63778fb40f8fe3d1a5ac3c0caeb35adad2f5cdc9
pzhang <1063764688@qq.com>
2023-10-16 16:59:26 +0800

    feat: 新闻资讯页

--- mock/newsList.js
+++ mock/newsList.js
@@ -1,32 +1,89 @@
 export default {
-  "errCode": 0,
-  "errMsg": "查询成功",
-  "data": [{
-    "_id": "63c38e81e1a35ce43a2689e4",
-    "title": "白茶喝散茶不好吗? 压成茶饼之后，又有什么好处?",
-    "is_essence": true,
-    "view_count": 5969,
-    "author": "豪吃的美食",
-    "publish_date": 1672486225000,
-    "picurl": "https://mp-0cb878b7-99ec-44ea-8246-12b123304b05.cdn.bspapp.com/cloudstorage/0cf43f6a-ea0f-499b-a355-15a9e2486bfc.jpg"
-  }, {
-    "_id": "63c38d3a819ce8640c86fc46",
-    "title": "白茶丨时光荏苒，岁月如梭，你收藏的老白茶发生了哪些变化？",
-    "is_essence": false,
-    "view_count": 328,
-    "author": "坤呈昊茶业",
-    "publish_date": 1672455088000,
-    "picurl": "https://mp-0cb878b7-99ec-44ea-8246-12b123304b05.cdn.bspapp.com/cloudstorage/2cb52829-de65-4816-82c3-a6ad9152f9aa.jpg"
-  }, {
-    "_id": "63c38cadf43e607808e6243e",
-    "title": "传承技艺茶叶飘香 今年我国茶叶总产值预计首超3000亿元",
-    "is_essence": true,
-    "view_count": 836,
-    "author": "海外网",
-    "publish_date": 1672445597000,
-    "picurl": "https://mp-0cb878b7-99ec-44ea-8246-12b123304b05.cdn.bspapp.com/cloudstorage/ae1ca356-3d90-4ac4-825d-53da55359e5c.jpg"
-  }],
-  "total": 11,
-  "timeCost": 63,
-  "author": "接口提供者：B站咸虾米_"
-}
+  errCode: 0,
+  errMsg: "查询成功",
+  data: [
+    {
+      _id: "63c38e81e1a35ce43a2689e4",
+      title: "白茶喝散茶不好吗? 压成茶饼之后，又有什么好处?",
+      is_essence: true,
+      view_count: 5969,
+      author: "豪吃的美食",
+      publish_date: 1672486225000,
+      picurl:
+        "https://mp-0cb878b7-99ec-44ea-8246-12b123304b05.cdn.bspapp.com/cloudstorage/0cf43f6a-ea0f-499b-a355-15a9e2486bfc.jpg",
+    },
+    {
+      _id: "63c38d3a819ce8640c86fc46",
+      title: "白茶丨时光荏苒，岁月如梭，你收藏的老白茶发生了哪些变化？",
+      is_essence: false,
+      view_count: 328,
+      author: "坤呈昊茶业",
+      publish_date: 1672455088000,
+      picurl:
+        "https://mp-0cb878b7-99ec-44ea-8246-12b123304b05.cdn.bspapp.com/cloudstorage/2cb52829-de65-4816-82c3-a6ad9152f9aa.jpg",
+    },
+    {
+      _id: "63c38cadf43e607808e6243e",
+      title: "传承技艺茶叶飘香 今年我国茶叶总产值预计首超3000亿元",
+      is_essence: true,
+      view_count: 836,
+      author: "海外网",
+      publish_date: 1672445597000,
+      picurl:
+        "https://mp-0cb878b7-99ec-44ea-8246-12b123304b05.cdn.bspapp.com/cloudstorage/ae1ca356-3d90-4ac4-825d-53da55359e5c.jpg",
+    },
+    {
+      _id: "63c38bc7e1a35ce43a26182c",
+      title: "老白茶很好，这样喝更养生",
+      is_essence: false,
+      view_count: 668,
+      author: "神州茶韵",
+      publish_date: 1672398586000,
+      picurl:
+        "https://mp-0cb878b7-99ec-44ea-8246-12b123304b05.cdn.bspapp.com/cloudstorage/efd1b13a-9790-487a-898f-15e6b8811302.jpg",
+    },
+    {
+      _id: "63c38acb819ce8640c86858d",
+      title: "存老白茶一定要注意这5个细节，不然可能就存废了！",
+      is_essence: false,
+      view_count: 98,
+      author: "懂茶帝",
+      publish_date: 1672396473000,
+      picurl:
+        "https://mp-0cb878b7-99ec-44ea-8246-12b123304b05.cdn.bspapp.com/cloudstorage/2650a910-ca28-406d-97bf-6f34b9cbb79b.jpg",
+    },
+    {
+      _id: "63c38db1f43e607808e657c6",
+      title: "经常喝白茶，想生病都难！",
+      is_essence: true,
+      view_count: 3689,
+      author: "黑白茶文化",
+      publish_date: 1672390386000,
+      picurl:
+        "https://mp-0cb878b7-99ec-44ea-8246-12b123304b05.cdn.bspapp.com/cloudstorage/a9a9d627-d660-42b6-9fd2-e68708916550.jpg",
+    },
+    {
+      _id: "63c38e1bf5cf3a23db32ad59",
+      title: "年底了，教你盖碗泡白茶的7点技巧，过年回家给家里人露一手",
+      is_essence: false,
+      view_count: 566,
+      author: "小陈茶事",
+      publish_date: 1672386884000,
+      picurl:
+        "https://mp-0cb878b7-99ec-44ea-8246-12b123304b05.cdn.bspapp.com/cloudstorage/ea9cb62b-7f75-457c-82c5-b14c2aad518e.jpg",
+    },
+    {
+      _id: "63c39b65819ce8640c898af8",
+      title: "今年茶叶总产值预计首超3000亿元",
+      is_essence: false,
+      view_count: 253,
+      author: "央视新闻",
+      publish_date: 1672323604000,
+      picurl:
+        "https://mp-0cb878b7-99ec-44ea-8246-12b123304b05.cdn.bspapp.com/cloudstorage/477d11b6-5259-4168-b9fb-83ea97ba7322.jpg",
+    },
+  ],
+  total: 11,
+  timeCost: 65,
+  author: "接口提供者：B站咸虾米_",
+};
--- pages/news/news.js
+++ pages/news/news.js
@@ -1,18 +1,51 @@
 // pages/news/news.js
+import {
+  getNewsList
+} from "../../service/api"
+import {
+  formatNum,
+  formatTime
+} from "../../utils/util"
+
 Page({
 
   /**
    * 页面的初始数据
    */
   data: {
-
+    newsList: [],
+    loading: false,
+    isEmpty: false
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad(options) {
+    this.getNewsData()
+  },
 
+  getNewsData(size = 0) {
+    this.setData({
+      loading: true
+    })
+    getNewsList({
+      limit: 8,
+      size: size
+    }).then(res => {
+      const data = res.data.map(o => {
+        o.view_count = formatNum(o.view_count)
+        o.publish_date = formatTime(o.publish_date, 5)
+        return o
+      });
+      let oldData = this.data.newsList;
+      let newData = [...oldData, ...data];
+      this.setData({
+        newsList: newData,
+        loading: false,
+        isEmpty: newData.length === res.total ? true : false
+      })
+    })
   },
 
   /**
@@ -54,7 +87,8 @@ Page({
    * 页面上拉触底事件的处理函数
    */
   onReachBottom() {
-
+    if (this.data.isEmpty) return;
+    this.getNewsData(this.data.newsList.length);
   },
 
   /**
