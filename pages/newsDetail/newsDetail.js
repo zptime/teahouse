07c57eb646a2edf69c2e78db9f335c069ca3103c
pzhang <1063764688@qq.com>
2023-10-17 10:29:33 +0800

    feat: 分享事件执行

--- pages/newsDetail/newsDetail.js
+++ pages/newsDetail/newsDetail.js
@@ -1,99 +1,100 @@
 // pages/newsDetail/newsDetail.js
 import {
   queryNewsDetail
-} from "../../service/api"
+} from "../../service/api";
 import {
   formatNum,
   formatTime
-} from "../../utils/util"
+} from "../../utils/util";
 
 Page({
-
   /**
    * 页面的初始数据
    */
   data: {
-    id: '',
-    detail: {}
+    id: "",
+    detail: {},
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad(options) {
-    console.log(options)
+    console.log(options);
     const {
       id
-    } = options
+    } = options;
     if (id) {
       this.setData({
-        id: options.id
-      })
-      this.getNewsDetail()
+        id: options.id,
+      });
+      this.getNewsDetail();
     }
   },
 
   getNewsDetail() {
-    queryNewsDetail(this.data.id).then(res => {
-      res.data.publish_date = formatTime(res.data.publish_date, 6)
-      res.data.view_count = formatNum(res.data.view_count)
+    queryNewsDetail(this.data.id).then((res) => {
+      res.data.publish_date = formatTime(res.data.publish_date, 6);
+      res.data.view_count = formatNum(res.data.view_count);
       res.data.content = res.data.content.replace(/<p/gi, "<p class='pstyle'");
-      res.data.content = res.data.content.replace(/<img/gi, "<img class='imgstyle'");
+      res.data.content = res.data.content.replace(
+        /<img/gi,
+        "<img class='imgstyle'"
+      );
       // wx.setNavigationBarTitle({
       //   title: res.data.title
       // })
       this.setData({
-        detail: res.data
-      })
-    })
+        detail: res.data,
+      });
+    });
   },
 
   /**
    * 生命周期函数--监听页面初次渲染完成
    */
-  onReady() {
-
-  },
+  onReady() {},
 
   /**
    * 生命周期函数--监听页面显示
    */
-  onShow() {
-
-  },
+  onShow() {},
 
   /**
    * 生命周期函数--监听页面隐藏
    */
-  onHide() {
-
-  },
+  onHide() {},
 
   /**
    * 生命周期函数--监听页面卸载
    */
-  onUnload() {
-
-  },
+  onUnload() {},
 
   /**
    * 页面相关事件处理函数--监听用户下拉动作
    */
-  onPullDownRefresh() {
-
-  },
+  onPullDownRefresh() {},
 
   /**
    * 页面上拉触底事件的处理函数
    */
-  onReachBottom() {
-
-  },
+  onReachBottom() {},
 
   /**
    * 用户点击右上角分享
    */
   onShareAppMessage() {
+    return {
+      title: this.data.detail.title,
+      path: "/pages/newsDetail/newsDetail?id=" + this.data.detail._id,
+    };
+  },
 
-  }
-})
+  // 分享到朋友圈
+  onShareTimeline() {
+    return {
+      title: this.data.detail.title,
+      path: "/pages/newsDetail/newsDetail?id=" + this.data.detail._id,
+    };
+  },
+});
